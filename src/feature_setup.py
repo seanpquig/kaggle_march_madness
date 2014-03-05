d62b4a810c63f5dc1face16c8d95e4ccbc8d831f
seanpquig <seanpquig@gmail.com>
2014-03-05 03:21:20 -0500

    incorporated proper traing set with seasons A-M

--- src/feature_setup.py
+++ src/feature_setup.py
@@ -9,10 +9,6 @@ import numpy as np
 from sklearn import linear_model, feature_selection, metrics
 
 
-def get_training_set(df):
-    """  """
-
-
 def get_reg_season_data(df):
     """ Adds regular season data to input DataFrame. """
 
@@ -98,7 +94,7 @@ def get_tourney_results(df):
             results.append(None)
             daynum.append(None)
 
-    df['results'] = results
+    df['result'] = results
     df['daynum'] = daynum
     return df
 
@@ -108,50 +104,59 @@ def main():
     # set up core DataFrame object for data transformation and analysis
     df = pd.read_csv('../data/sample_submission.csv')
     del df['pred']
-    
+
+    df_train = pd.read_csv('../data/training_labels.csv')
+    df_train.columns = ['id', 'result_excel']
+
     # break out lookup keys
-    df['season'] = [i[0] for i in df.id]
-    df['team1'] = [i[2:5] for i in df.id]
-    df['team2'] = [i[6:] for i in df.id]
-    
+    df['season'], df_train['season'] = [i[0] for i in df.id], [i[0] for i in df_train.id]
+    df['team1'], df_train['team1'] = [i[2:5] for i in df.id], [i[2:5] for i in df_train.id]
+    df['team2'], df_train['team2'] = [i[6:] for i in df.id], [i[6:] for i in df_train.id]
+
     # pull in regular season data
     df = get_reg_season_data(df)
+    df_train = get_reg_season_data(df_train)
     
     # get tourney seed data
     df = get_seeds(df)
+    df_train = get_seeds(df_train)
 
     # pull in tourney results as output variable
     df = get_tourney_results(df)
+    df_train = get_tourney_results(df_train)
 
     # calculated features
-    df['seed_diff'] = df['team1_seed'] - df['team2_seed']
+    # df['seed_diff'] = df['team1_seed'] - df['team2_seed']
 
     # Restrict to real games and exculde play-in games
-    df_test = df[ df['results'].notnull() & (df['daynum'] > 135) ]
+    df_test = df[ df['result'].notnull() & (df['daynum'] > 135) ]
 
 
-    ### SETUP FEATURES AND OUTPUT VARIABLE
-    # features = ['team1_wins', 'team2_wins', 'team1_losses', 'team2_losses']
-    features = ['team1_seed', 'team2_seed']
-    x_vals = df_test[features]
-    y_vals = df_test.results.values.astype(int)
+    ### SETUP FEATURES AND DEPENDENT VARIABLE
+    features = ['team1_wins', 'team2_wins', 'team1_losses', 'team2_losses', 
+                'team1_seed', 'team2_seed']
+    X_test = df_test[features]
+    y_test = df_test.result.values.astype(int)
+
+    X_train = df_train[features]
+    y_train = df_train.result.values.astype(int)
 
 
     ### CREATE MODEL
-    model = linear_model.LogisticRegression().fit(x_vals, y_vals)
-    y_pred = model.predict(x_vals)
-    y_pred_probs = model.predict_proba(x_vals)
+    model = linear_model.LogisticRegression().fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_pred_probs = model.predict_proba(X_test)
 
-    print '\n', 'features: ', x_vals.columns.values
-    print 'p-vals: ', feature_selection.univariate_selection.f_classif(x_vals, y_vals)[1]
+    print '\n', 'features: ', X_test.columns.values
+    print 'p-vals: ', feature_selection.univariate_selection.f_classif(X_test, y_test)[1]
     print 'coefs: ', model.coef_
 
-    print '\n', 'conf. matrix: ', '\n', metrics.confusion_matrix(y_vals, y_pred)
-    # print '\n', 'clasif. report: ', '\n', metrics.classification_report(y_vals, y_pred)
-    fpr, tpr, thresholds = metrics.roc_curve(y_vals, y_pred, pos_label=1)
+    print '\n', 'conf. matrix: ', '\n', metrics.confusion_matrix(y_test, y_pred)
+    # print '\n', 'clasif. report: ', '\n', metrics.classification_report(y_test, y_pred)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred, pos_label=1)
     print '\n', 'AUC: ', metrics.auc(fpr, tpr)
-    print 'mean-acc: ', model.score(x_vals, y_vals)
-    print 'LogLoss:  ', metrics.log_loss(y_vals, y_pred_probs)
+    print 'mean-acc: ', model.score(X_test, y_test)
+    print 'LogLoss:  ', metrics.log_loss(y_test, y_pred_probs)
 
 
     ### OUTPUT PREDICTIONS
