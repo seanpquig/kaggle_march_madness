f95da59035ca31bb3b11465f119c6dcf3beadbda
seanpquig <seanpquig@gmail.com>
2014-03-07 01:26:15 -0500

    set up cross validation and tried different kinds of classifiers

--- src/model.py
+++ src/model.py
@@ -6,7 +6,9 @@ Script for preparing and transforming features from given data sets
 import code
 import pandas as pd
 import numpy as np
-from sklearn import linear_model, feature_selection, metrics
+from sklearn import linear_model, naive_bayes, svm, feature_selection, metrics, cross_validation
+from sklearn.ensemble.forest import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
 
 
 def get_reg_season_data(df):
@@ -68,7 +70,7 @@ def get_seeds(df):
 
 
 def get_tourney_results(df):
-    """ Pulls tourney results into input df as output variable. """
+    """ Adds tourney results to input DataFrame as output variable. """
 
     tourney_results_df = pd.read_csv('../data/tourney_results.csv')
     tourney_results = {}
@@ -99,16 +101,50 @@ def get_tourney_results(df):
     return df
 
 
+def print_model_stats(model, X, y, msg):
+    print '\n', msg
+    y_pred = model.predict(X)
+    y_pred_probs = model.predict_proba(X)
+
+    print '  conf. matrix: ', '\n ', metrics.confusion_matrix(y, y_pred)
+    # print '\n', 'clasif. report: ', '\n', metrics.classification_report(y_test, y_pred)
+    print '  AUC:  ', metrics.roc_auc_score(y, y_pred)
+    print '  mean-acc: ', model.score(X, y)
+    print '  LogLoss:  ', metrics.log_loss(y, y_pred_probs)
+
+
+def print_cross_val_stats(model, features, labels):
+    # Cross Validation
+    AUCs, MAs, LogLosses = [], [], []
+    np.random.seed(1234)
+    for i in xrange(20):
+        X_train, X_test, y_train, y_test = \
+            cross_validation.train_test_split(features, labels, test_size=.35)
+
+        y_pred_test = model.fit(X_train, y_train).predict(X_test)
+        y_pred_probs_test = model.predict_proba(X_test)
+        fpr_test, tpr_test, thresholds_test = metrics.roc_curve(y_test, y_pred_test, pos_label=1)
+
+        AUCs.append(metrics.auc(fpr_test, tpr_test))
+        MAs.append(model.score(X_test, y_test))
+        LogLosses.append(metrics.log_loss(y_test, y_pred_probs_test))
+
+    print '\nCross-validation:'
+    print '  average AUC: ', np.mean(AUCs)
+    print '  range AUC: ', min(AUCs), '-', max(AUCs)
+    print '  average mean-acc:  ', np.mean(MAs)
+    print '  range mean-acc:  ', min(MAs), '-', max(MAs)
+    print '  average LogLoss:  ', np.mean(LogLosses)
+    print '  range LogLoss:  ', min(LogLosses), '-', max(LogLosses)
+
 
 def main():
-    # set up core DataFrame object for data transformation and analysis
+    # set up core DataFrame objects for data transformation and analysis
     df = pd.read_csv('../data/sample_submission.csv')
+    df_train = pd.read_csv('../data/training_labels.csv')
     del df['pred']
 
-    df_train = pd.read_csv('../data/training_labels.csv')
-    df_train.columns = ['id', 'result_excel']
-
-    # break out lookup keys
+    # create lookup key columns
     df['season'], df_train['season'] = [i[0] for i in df.id], [i[0] for i in df_train.id]
     df['team1'], df_train['team1'] = [i[2:5] for i in df.id], [i[2:5] for i in df_train.id]
     df['team2'], df_train['team2'] = [i[6:] for i in df.id], [i[6:] for i in df_train.id]
@@ -125,46 +161,50 @@ def main():
     df = get_tourney_results(df)
     df_train = get_tourney_results(df_train)
 
-    # calculated features
-    # df['seed_diff'] = df['team1_seed'] - df['team2_seed']
-
-    # Restrict to real games and exculde play-in games
-    df_test = df[ df['result'].notnull() & (df['daynum'] > 135) ]
+    # Restrict to real matchups and exculde play-in games
+    df_score = df[ (df['result'].notnull()) & (df['daynum'] > 135) ]
+    df_train = df_train[ df_train['daynum'] > 135 ]
 
 
-    ### SETUP FEATURES AND DEPENDENT VARIABLE
+    ### SETUP FEATURES AND DEPENDENT VARIABLE DATA
     features = ['team1_wins', 'team2_wins', 'team1_losses', 'team2_losses', 
                 'team1_seed', 'team2_seed']
-    X_test = df_test[features]
-    y_test = df_test.result.values.astype(int)
+    train_features = df_train[features]
+    train_labels = df_train.result.values.astype(int)
 
-    X_train = df_train[features]
-    y_train = df_train.result.values.astype(int)
+    np.random.seed(1)
+    X_train, X_test, y_train, y_test = \
+        cross_validation.train_test_split(train_features, train_labels, test_size=.35)
 
+    X_score = df_score[features]
+    y_score = df_score.result.values.astype(int)
 
-    ### CREATE MODEL
+    
+    ### CREATE MODEL AND RUN STATISTICS
     model = linear_model.LogisticRegression().fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    y_pred_probs = model.predict_proba(X_test)
+    # model = RandomForestClassifier(n_estimators=50, max_depth = 3).fit(train_features, train_labels)
+    # model = ExtraTreesClassifier(n_estimators=500, max_depth = 3).fit(X_train, y_train)
+    # model = AdaBoostClassifier().fit(X_train, y_train)
+    # model = svm.SVC(probability=True).fit(X_train, y_train)
+    # model = naive_bayes.GaussianNB().fit(X_train, y_train)
 
-    print '\n', 'features: ', X_test.columns.values
-    print 'p-vals: ', feature_selection.univariate_selection.f_classif(X_test, y_test)[1]
+    print '\n', 'features: ', features
+    print 'p-vals: ', feature_selection.univariate_selection.f_classif(X_train, y_train)[1]
+    code.interact(local=locals())
     print 'coefs: ', model.coef_
 
-    print '\n', 'conf. matrix: ', '\n', metrics.confusion_matrix(y_test, y_pred)
-    # print '\n', 'clasif. report: ', '\n', metrics.classification_report(y_test, y_pred)
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred, pos_label=1)
-    print '\n', 'AUC: ', metrics.auc(fpr, tpr)
-    print 'mean-acc: ', model.score(X_test, y_test)
-    print 'LogLoss:  ', metrics.log_loss(y_test, y_pred_probs)
+    print_model_stats(model, X_train, y_train, 'training set:')
+    print_model_stats(model, X_test, y_test, 'testing set:')
+    print_model_stats(model, X_score, y_score, 'scored set:')
+    print_cross_val_stats(model, train_features, train_labels)
 
 
     ### OUTPUT PREDICTIONS
-    x_vals = df[features]
-    y_pred_probs = model.predict_proba(x_vals)
+    X_vals = df[features]
+    y_pred_probs = model.predict_proba(X_vals)
     df['pred'] = y_pred_probs.T[1]
-    print '\nwriting csv output...'
-    df[['id', 'pred']].to_csv('../submissions/output.csv', index=False)
+    # print '\nwriting csv output...'
+    # df[['id', 'pred']].to_csv('../submissions/output.csv', index=False)
 
 
 if __name__ == '__main__':
